eb6b5a63fd96cc8c2baae3d27012c84dfcfc3995
pranalisakhare <pranali2999@gmail.com>
2022-01-29 13:27:03 +0530

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./component/usercard";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: false };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      3000
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navigation">
+            <h2>LetsGrowMore</h2>
+            <p>TASK 2</p>
+            <button className="fetchbtn" onClick={this.updateState}>
+              Get Users
+            </button>
+          </div>
+        </nav>
+
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+
+      </>
+    );
+  }
 }
-
 export default App;
